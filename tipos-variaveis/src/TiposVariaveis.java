277de14c5e01f28f5ad936851486a928f5fa3a8c
Nickolas Demarchi <nickolasdemarchi@gmail.com>
2025-02-04 20:36:05 -0300

    Tipos e variáveis completo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        byte idade = 20;
+        short ano = 2025;
+        int cep = 21070333;
+        long cpf = 98765432109L; // long precisa terminar com L
+        float pi = 3.14F; // float precisa terminar com F
+        double salario = 1275.33;
+        //constante
+        final double PI = 3.14; //constantes são declaradas com "final" e serem escritas em caixa alta
     }
 }
